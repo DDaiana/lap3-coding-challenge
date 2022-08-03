9b1d4d7e25736b30ed5f11b8872aef1ff98e0cf1
uzair <uzair1200@hotmail.co.uk>
2022-08-03 12:13:01 +0100

    react settup

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className='landing-page-container'>
+        <div className='left-side'>
+          <form className='form'>
+            <input className='input' value = {username} placeholder = "Username" onChange={e => setUsername(e.target.value)} />
+            <button className='button' onClick={handleSubmit}>{loading ? "Searching..." : "Search"} </button>
+          </form>
+        </div>
+
+      </div>
     </div>
   );
 }
